b5084cecf8722596be76a94d5055af1c43682dd7
Guilherme G Likoski <159035705+GuilhermeGLikoski@users.noreply.github.com>
2026-03-13 10:47:56 -0300

    Create exercicio2.py
    exercício 2) Escreva um algoritmo em py para calcular o valor, em reais, que deve ser pago por um cliente de uma locadora de carros. Sabe se que:
    
    a. O valor de locação de um cada carro é 100.00 reais;
    
    b. O cliente pode locar um único carro por vários dias.

---
+++ exercicio2.py
@@ -0,0 +1,6 @@
+taxa_diaria = 100.00
+
+dias_locacao = int(input("Por quantos dias você irá alugar o carro? "))
+valor_total = dias_locacao * taxa_diaria
+
+print(f"o valor total da locação será de R$ {valor_total:.2f}")
